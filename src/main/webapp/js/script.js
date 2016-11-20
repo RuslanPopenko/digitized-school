155cb12553eac0fe7c94ac005cf725e9155bc6a0
Ruslan Popenko <ruslanpopenko@ukr.net>
2016-11-20 15:33:18 +0200

    Example send ajax for Person.

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,3 +1,29 @@
 /**
  * Created by ruslan on 31.10.16.
  */
+
+function createPerson() {
+    var data = {};
+    data['firstName'] = $('#firstName').val();
+    data['lastName'] = $('#lastName').val();
+    data['age'] = $('#age').val();
+    data['sex'] = $('#sex').val();
+    var url = '/api/persons';
+    var method = 'POST';
+
+    sendAjax(url, method, data, function (data) {
+        $('#response').html('Created person with id = ' + data.id);
+    });
+
+};
+
+function sendAjax(url, method, data, callback) {
+    $.ajax({
+        url: url,
+        type: method,
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        success: callback
+    });
+};
