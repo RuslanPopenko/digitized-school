7f53094d6f34ab3552ef0d1c45c363efed98c169
Max-Morozov <frost15@ukr.net>
2016-11-26 13:07:20 +0200

    Add functional for Subject entity.

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -17,20 +17,6 @@ function createPerson() {
     });
 };
 
-function updatePerson() {
-    var data = {};
-    data['firstName'] = $('#firstNameU').val();
-    data['lastName'] = $('#lastNameU').val();
-    data['age'] = $('#ageU').val();
-    data['sex'] = $('#sexU').val();
-    var url = '/api/persons/'+$('#idU').val();
-    var method = 'PUT';
-
-    sendAjax(url, method, data, function (data) {
-        $('#responseUpdate').html('Updated person with id = ' + data.id);
-    });
-};
-
 function getPerson() {
     var url = '/api/persons/'+$('#id').val();
     var method = 'GET';
@@ -50,10 +36,24 @@ function getAllPersons() {
         for (var i = 0; i < data.length; i++)
             output = output.concat('Person ' + data[i].id + ': '
                 + data[i].firstName + ' ' + data[i].lastName
-                + ', age = ' + data[i].age + ', sex = ' + data[i].sex + "\n")
+                + ', age = ' + data[i].age + ', sex = ' + data[i].sex + ' | ')
         $('#responseGetAll').html(output);
     });
 }
+
+function updatePerson() {
+    var data = {};
+    data['firstName'] = $('#firstNameU').val();
+    data['lastName'] = $('#lastNameU').val();
+    data['age'] = $('#ageU').val();
+    data['sex'] = $('#sexU').val();
+    var url = '/api/persons/'+$('#idU').val();
+    var method = 'PUT';
+
+    sendAjax(url, method, data, function (data) {
+        $('#responseUpdate').html('Updated person with id = ' + data.id);
+    });
+};
 
 function deletePerson() {
     var url = '/api/persons/'+$('#idD').val();
@@ -81,19 +81,13 @@ function createPupil() {
                 var str = $('#subject_ids').val();
                 var ids = str.split(',');
                 pupilData['subjects'] = [];
-                for (var i = 0; i < ids.length; i++) {
-                    url = '/api/subjects/'+ids[i];
-                    sendAjax(url, method, null, function (data) {
-                        pupilData['subjects'].push(data);
-                        if (i == ids.length-1) {
-                            url = '/api/pupils';
-                            method = 'POST';
-                            sendAjax(url, method, pupilData, function (data) {
-                                $('#responseCreate').html('Created pupil with id = ' + data.id);
-                            });
-                        }
-                    });
-                }
+                url = '/api/subjects/';
+                getRequestRecursive(url, ids, pupilData['subjects'], function(data){
+                    url = '/api/pupils';
+                    sendAjax(url, 'POST', pupilData, function(data) {
+                        $('#responseCreate').html('Created pupil with id = ' + data.id);
+                    })
+                });
             });
         });
     });
@@ -104,8 +98,8 @@ function getPupil() {
     var method = 'GET';
 
     sendAjax(url, method, null, function (data) {
-        var output = 'Pupil ' + data.id + ': person ' + data.person.id + ', school '
-            + data.school.id + ', schoolClass ' + data.schoolClass.id + ', subjects';
+        var output = 'Pupil ' + data.id + ': personId = ' + data.person.id + ', schoolId = '
+            + data.school.id + ', schoolClassId = ' + data.schoolClass.id + ', subjects =';
         for (var i = 0; i < data.subjects.length; i++)
             output = output.concat(' ' + data.subjects[i].id);
         $('#responseGet').html(output);
@@ -119,11 +113,11 @@ function getAllPupils() {
     sendAjax(url, method, null, function (data) {
         var output = '';
         for (var i = 0; i < data.length; i++) {
-            output = output.concat('Pupil ' + data[i].id + ': person ' + data[i].person.id + ', school '
-                + data[i].school.id + ', schoolClass ' + data[i].schoolClass.id + ', subjects');
+            output = output.concat('Pupil ' + data[i].id + ': personId = ' + data[i].person.id + ', schoolId = '
+                + data[i].school.id + ', schoolClassId = ' + data[i].schoolClass.id + ', subjectsIds =');
             for (var j = 0; j < data[i].subjects.length; j++)
                 output = output.concat(' ' + data[i].subjects[j].id);
-            output = output.concat("\n");
+            output = output.concat(' | ');
         }
         $('#responseGetAll').html(output);
     });
@@ -145,19 +139,13 @@ function updatePupil() {
                 var str = $('#subject_idsU').val();
                 var ids = str.split(',');
                 pupilData['subjects'] = [];
-                for (var i = 0; i < ids.length; i++) {
-                    url = '/api/subjects/'+ids[i];
-                    sendAjax(url, method, null, function (data) {
-                        pupilData['subjects'].push(data);
-                        if (i == ids.length-1) {
-                            url = '/api/pupils/'+$('#idU').val();
-                            method = 'PUT';
-                            sendAjax(url, method, pupilData, function (data) {
-                                $('#responseCreate').html('Updated pupil with id = ' + data.id);
-                            });
-                        }
-                    });
-                }
+                url = '/api/subjects/';
+                getRequestRecursive(url, ids, pupilData['subjects'], function(data){
+                    url = '/api/pupils/'+$('#idU').val();
+                    sendAjax(url, 'PUT', pupilData, function(data) {
+                        $('#responseUpdate').html('Updated pupil with id = ' + data.id);
+                    })
+                });
             });
         });
     });
@@ -168,13 +156,67 @@ function deletePupil() {
     var method = 'DELETE';
 
     sendAjax(url, method, null, function (data) {
-        var output = 'Deleted pupil ' + data.id + ': person ' + data.person.id + ', school '
-            + data.school.id + ', schoolClass ' + data.schoolClass.id + ', subjects';
+        var output = 'Deleted pupil ' + data.id + ': personId = ' + data.person.id + ', schoolId = '
+            + data.school.id + ', schoolClassId = ' + data.schoolClass.id + ', subjectsIds =';
         for (var i = 0; i < data.subjects.length; i++)
             output = output.concat(' ' + data.subjects[i].id);
         $('#responseDelete').html(output);
     });
 }
+
+function createSubject() {
+    var data = {};
+    data['name'] = $('#name').val();
+    var url = '/api/subjects';
+    var method = 'POST';
+
+    sendAjax(url, method, data, function (data) {
+        $('#responseCreate').html('Created subject with id = ' + data.id);
+    });
+};
+
+function getSubject() {
+    var url = '/api/subjects/'+$('#id').val();
+    var method = 'GET';
+
+    sendAjax(url, method, null, function (data) {
+        $('#responseGet').html('Subject ' + data.id + ': ' + data.name);
+    });
+}
+
+function getAllSubjects() {
+    var url = '/api/subjects';
+    var method = 'GET';
+
+    sendAjax(url, method, null, function (data) {
+        var output = '';
+        for (var i = 0; i < data.length; i++)
+            output = output.concat('Subject ' + data[i].id + ': ' + data[i].name + ' | ')
+        $('#responseGetAll').html(output);
+    });
+}
+
+function updateSubject() {
+    var data = {};
+    data['name'] = $('#nameU').val();
+    var url = '/api/subjects/'+$('#idU').val();
+    var method = 'PUT';
+
+    sendAjax(url, method, data, function (data) {
+        $('#responseUpdate').html('Updated subject with id = ' + data.id);
+    });
+};
+
+function deleteSubject() {
+    var url = '/api/subjects/'+$('#idD').val();
+    var method = 'DELETE';
+
+    sendAjax(url, method, null, function (data) {
+        $('#responseDelete').html('Deleted subject:' + data.name);
+    });
+}
+
+//---------------------------------------------------------------
 
 function getRequestRecursive(url, ids, accumulator, callback) {
     sendAjax(url + ids.pop(), 'GET', null, function (data) {
@@ -185,7 +227,6 @@ function getRequestRecursive(url, ids, accumulator, callback) {
             callback();
         }
     });
-
 }
 
 function sendAjax(url, method, data, callback) {
